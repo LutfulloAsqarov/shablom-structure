efa0b83f798b6fe6ed0c885b3fc5a50f8444592a
LutfulloAsqarov <lutfulloasqarov9@gmail.com>
2024-02-19 16:53:57 +0500

    newshablon

--- js/main.js
+++ js/main.js
@@ -1,28 +1,42 @@
-let navBar = document.getElementById("header__nav__hamburger-btn");
-let navList = document.getElementById("header__nav-list");
-let backtop = document.getElementById("backtop");
-let navShrink = document.getElementById("navbar");
+let backTop = document.querySelector(".back-top");
+let header = document.querySelector(".header");
+let body = document.querySelector("body");
+let darkMode = document.querySelector(".header__nav__right__btns__dark-mode");
+let headerList = document.querySelector(".header__nav__right__list");
+let hamburger = document.querySelector(".header__nav__right__btns__hamburger");
+let closeHeaderList = document.querySelector(
+    ".header__nav__right__list__close"
+);
+
 
 window.addEventListener("scroll", function () {
-    showBacktop();
-    showNavShrink();
+    showBackTop();
+    showHeaderShrink();
 });
 
-navBar.addEventListener("click", function () {
-    navList.classList.toggle("show-header__nav-list");
+darkMode.addEventListener("click", function(){
+    body.classList.toggle("dark");
+})
+
+hamburger.addEventListener("click", function(){
+    headerList.classList.toggle("show__header__nav__right__list");
+})
+
+closeHeaderList.addEventListener("click", function () {
+    headerList.classList.remove("show__header__nav__right__list");
 });
 
-function showBacktop() {
-    if (scrollY > 200) {
-        backtop.classList.add("show-backtop");
+function showBackTop() {
+    if (scrollY > 100) {
+        backTop.classList.add("show__back-top");
     } else {
-        backtop.classList.remove("show-backtop");
+        backTop.classList.remove("show__back-top");
     }
 }
-function showNavShrink() {
+function showHeaderShrink() {
     if (scrollY > 0) {
-        navShrink.classList.add("show-navshrink");
+        header.classList.add("show__header__shrink");
     } else {
-        navShrink.classList.remove("show-navshrink");
+        header.classList.remove("show__header__shrink");
     }
 }
